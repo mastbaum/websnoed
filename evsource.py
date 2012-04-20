6e515c45ccf379a31dd654a280a1c87095dc77bb
Andy Mastbaum <amastbaum@gmail.com>
2012-04-19 23:48:33 -0400

    add screen cap

--- evsource.py
+++ evsource.py
@@ -1,42 +1,24 @@
 import time
+import pickle
 import couchdb
 import numpy
 
-# random and using couch for now
-# eventually read from a real source and serve json directly
+# using couch for now; eventually serve json directly
 
 couch = couchdb.Server('http://localhost:5984')
 db = couch['websnoed']
 
-gtid = 0
-nhit = 0
-while(True):
-    q = numpy.zeros((9500,), dtype=float)
-    t = numpy.zeros((9500,), dtype=float)
-    for i in range(9500):
-        if numpy.random.random() < 0.2:
-            q[i] = 4095 * numpy.random.random()
-            t[i] = 4095 * numpy.random.random()
-            nhit += 1
+events = []
+with open('events.pickle') as f:
+    events = pickle.load(f)
 
+for event in events[30:31]:
+    event['_id'] = 'data'
     if 'data' in db:
-        data = db['data']
-    else:
-        data = {'_id': 'data'}
+        event['_rev'] = db['data']['_rev']
 
-    data['t'] = list(t)
-    data['q'] = list(q)
+    db.save(event)
 
-    data['qhist'] = map((lambda x: map(float, x)), zip(*reversed(numpy.histogram(data['q']))))
-    data['thist'] = map((lambda x: map(float, x)), zip(*reversed(numpy.histogram(data['t']))))
-
-    data['gtid'] = gtid
-    gtid += 1
-
-    data['nhit'] = nhit
-    nhit = 0
-
-    db.save(data)
-
-    time.sleep(0.1)
+    time.sleep(1.0)
+    #raw_input()
 
